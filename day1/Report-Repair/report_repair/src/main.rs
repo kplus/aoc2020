b1f3b9bc208a3fb12c6f87592ea8cf92d9771cdb
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-01 13:19:48 +0000

    day1: initial solution using 2 for loops

--- day1/Report-Repair/report_repair/src/main.rs
+++ day1/Report-Repair/report_repair/src/main.rs
@@ -1,10 +1,27 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+static SUM: i32 = 2020;
+
+fn find_match(contents: String) -> (i32, i32) {
+    let vec: Vec<i32> = contents.lines().filter_map(|s| s.parse().ok()).collect();
+
+    for first in 0..vec.len() {
+        for second in first + 1..vec.len() {
+            if vec[first] + vec[second] == SUM {
+                return (vec[first], vec[second]);
+            }
+        }
+    }
+    (0, 0)
+}
+
 fn main() -> std::io::Result<()> {
     let mut file = File::open("../input.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    println!("{}", contents);
+
+    let (x, y) = find_match(contents);
+    println!("{}", x * y);
     Ok(())
 }
