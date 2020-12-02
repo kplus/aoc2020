0727b5f68d2a0197037d74fb0350e1265338587e
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-02 12:37:11 +0000

    day2: question 2

--- day2/password/src/main.rs
+++ day2/password/src/main.rs
@@ -16,6 +16,32 @@ fn check_pass(line: &str) -> bool {
     false
 }
 
+// return whether the password is valid
+// also print out the valid password string
+// for question 2
+fn check_pass2(line: &str) -> bool {
+    let whole: Vec<&str> = line.split(' ').collect();
+    let range: Vec<&str> = whole[0].split('-').collect();
+    let first: usize = range[0].parse().unwrap();
+    let second: usize = range[1].parse().unwrap();
+    let first_c = whole[2].chars().nth(first - 1).unwrap();
+    let second_c = whole[2].chars().nth(second - 1).unwrap();
+    let ch: char = whole[1].chars().next().unwrap();
+
+    if first_c == ch {
+        if second_c != ch {
+            println!("Find valid password line: {}", line);
+            return true;
+        }
+    } else if second_c == ch {
+        println!("Find valid password line: {}", line);
+        return true;
+    }
+    false
+}
+
+// change function call to check_pass for question 1
+// and change it to check_pass2 for question 2
 fn main() -> std::io::Result<()> {
     let mut file = File::open("../input.txt")?;
     let mut contents = String::new();
@@ -23,7 +49,7 @@ fn main() -> std::io::Result<()> {
 
     let mut count = 0;
     for line in contents.lines() {
-        if check_pass(line) {
+        if check_pass2(line) {
             count += 1;
         };
     }
