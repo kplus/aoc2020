50bf32ec87534b22549014247948ad8a874293b5
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-02 11:58:33 +0000

    add ignore file

--- day2/password/target/debug/deps/password-b9ae7e366b99ba57.d
+++
@@ -1,5 +0,0 @@
-/home/terry/work/wip-aoc2020/day2/password/target/debug/deps/password-b9ae7e366b99ba57: src/main.rs
-
-/home/terry/work/wip-aoc2020/day2/password/target/debug/deps/password-b9ae7e366b99ba57.d: src/main.rs
-
-src/main.rs:
--- day2/password/target/debug/password.d
+++
@@ -1,1 +0,0 @@
-/home/terry/work/wip-aoc2020/day2/password/target/debug/password: /home/terry/work/wip-aoc2020/day2/password/src/main.rs
