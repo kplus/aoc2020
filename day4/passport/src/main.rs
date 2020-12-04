897ab69ede9625b8dffbb8cc362860fb55b15ea2
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-04 12:53:30 +0000

    day4: question 1

--- day4/passport/src/main.rs
+++ day4/passport/src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 
 fn fill_hash(person: &str) -> HashMap<String, String> {
     let mut details = HashMap::new();
-    for items in person.split(' ') {
+    for items in person.split_whitespace() {
         let value: Vec<&str> = items.split(':').collect();
         details.insert(value[0].to_string(), value[1].to_string());
     }
@@ -18,13 +18,24 @@ fn load_file<P: AsRef<Path>>(path: P) -> Result<Vec<HashMap<String, String>>, Bo
 
     let mut out = Vec::new();
     for person in input.split("\n\n") {
-        //    println!("read in peron details:\n{}", person);
+        //println!("read in peron details:\n{}", person);
         out.push(fill_hash(person));
     }
     Ok(out)
 }
 
 fn check(id: HashMap<String, String>) -> bool {
+    let key_to_check = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    //println!("the id to check is {:#?}", id);
+    for f in key_to_check.iter() {
+        //println!("the key to check is {}", f);
+
+        if !id.contains_key(&f.to_string()) {
+            //println!("it missed the valid key, exit");
+            return false;
+        }
+    }
+    //println!("it seems all esstential key are here");
     true
 }
 
