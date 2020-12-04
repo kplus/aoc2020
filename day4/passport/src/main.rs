f25769dfec8437fcf0715debc7c8ae51afdf82d0
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-04 09:17:26 +0000

    day4: initial

---
+++ day4/passport/src/main.rs
@@ -0,0 +1,39 @@
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+struct ID {
+    byr: String, //Birth Year
+    iyr: String, //Issue Year
+    eyr: String, //Expiration Year
+    hgt: String, //Height
+    hcl: String, //Hair Color
+    ecl: String, //Eye Color
+    pid: String, //Passport ID
+    cid: String, //Country ID
+}
+
+fn load_file<P: AsRef<Path>>(path: P) -> Result<Vec<ID>, Box<dyn Error>> {
+    let input = fs::read_to_string(path)?;
+    println!("read in content:\n{}", input);
+
+    let id = ID {
+        byr: String::from("0"),
+        iyr: String::from("0"),
+        eyr: String::from("0"),
+        hgt: String::from("0cm"),
+        hcl: String::from("#fffffd"),
+        ecl: String::from("gry"),
+        pid: String::from("0"),
+        cid: String::from("0"),
+    };
+    let mut out = Vec::new();
+    out.push(id);
+    Ok(out)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let data = load_file("../input.txt")?;
+
+    Ok(())
+}
