8fca872da4ad9b25c9abf31d0623a6f86387ae46
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-06 08:58:26 +0000

    rename day folders to include program name

--- day2/password/target/rls/debug/deps/password-6dba19f552d6d6ad.d
+++
@@ -1,5 +0,0 @@
-/home/terry/work/wip-aoc2020/day2/password/target/rls/debug/deps/password-6dba19f552d6d6ad.rmeta: src/main.rs
-
-/home/terry/work/wip-aoc2020/day2/password/target/rls/debug/deps/password-6dba19f552d6d6ad.d: src/main.rs
-
-src/main.rs:
