9470b5c9fca5f0b76b666add240bf0899acba746
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-06 09:05:36 +0000

    day6: Initial

--- day6-custom/custom/src/main.rs
+++ day6-custom/custom/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+// [in]     Path of file to read personal details from
+// [out     Arrary of Hashmaps, each Hashmap stores full details of a single person
+fn load_file<P: AsRef<Path>>(path: P) -> Result<Vec<String>, Box<dyn Error>> {
+    let input = fs::read_to_string(path)?;
+    //println!("read in content:\n{}", input);
+
+    let mut out = Vec::new();
+    for person in input.split("\n\n") {
+        //println!("read in peron details:\n{}", person);
+        out.push(person.to_string());
+    }
+    Ok(out)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let data = load_file("../input.txt")?;
+
+    println!("{:#?}", data);
+
+    Ok(())
 }
