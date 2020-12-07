9747d71bf2efcd8108333cbcccd6b3cb3291dd8b
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-07 14:40:17 +0000

    day7: question1: main function stub

--- day7-bag/bag/src/main.rs
+++ day7-bag/bag/src/main.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use std::fs;
 use std::path::Path;
 
+#[derive(Debug)]
 struct BAG {
     //    name: String,
     father: Vec<String>,
@@ -26,25 +27,24 @@ impl BAG {
 
 fn get_relation(s: &str, bt: &HashMap<String, BAG>) -> (String, Vec<String>) {
     let bags: Vec<&str> = s.split("bag").collect();
+    //println!("bags are {:#?}", bags);
+
     let bag = "me".to_string();
     let mut v = Vec::new();
     v.push("1".to_string());
-
-    //println!("bags are {:#?}", bags);
-
     (bag, v)
 }
 
 fn build_tree(s: Vec<String>) -> HashMap<String, BAG> {
-    let mut bag_tree = HashMap::new();
+    let mut tree = HashMap::new();
 
     for bags in s.iter() {
         println!("The bag line is {}", bags);
-        let (bag, children) = get_relation(bags, &bag_tree);
+        let (bag, children) = get_relation(bags, &tree);
         let new_bags = BAG::new(bag.as_str());
-        bag_tree.insert(bag, new_bags);
+        tree.insert(bag, new_bags);
     }
-    bag_tree
+    tree
 }
 
 // [in]     Path of file to read details from
@@ -61,10 +61,31 @@ fn load_file<P: AsRef<Path>>(path: P) -> Result<Vec<String>, Box<dyn Error>> {
     Ok(out)
 }
 
+//todo: Get the number of bags which can contain the specified bag
+// [in]     The bag tree
+// [in]     The target bag name
+// [out]    Number of possible bags, or None if target bag name is invalid
+fn get_containers(s: &str, tree: HashMap<String, BAG>) -> Option<usize> {
+    Some(0)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let data = load_file("../input.txt")?;
     //println!("{:#?}", data);
+    let bag_name = "shiny gold";
+    let bag_tree = build_tree(data);
+    println!("bag tree built is {:#?}", bag_tree);
 
-    build_tree(data);
+    let count = get_containers(bag_name, bag_tree);
+    match count {
+        Some(x) => println!(
+            "There are {} bags that can contain at least 1 {}",
+            x, bag_name
+        ),
+        None => eprintln!(
+            "No bag name {} found, please check your input file.",
+            bag_name
+        ),
+    }
     Ok(())
 }
