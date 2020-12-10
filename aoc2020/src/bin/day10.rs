5b72dea6a2d08d3704ed19f3decfc4a159f85461
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-10 11:02:00 +0000

    day10: question2: debugging run but incorrect answer

--- aoc2020/src/bin/day10.rs
+++ aoc2020/src/bin/day10.rs
@@ -1,13 +1,28 @@
 use std::error::Error;
 
-fn question2(mut _s: Vec<usize>) -> Result<[usize; 3], &'static str> {
-    Err("Cannot find the window to fit target number.")
+// f(n)=f(n-1) * e(n-1) + f(n-2) * e(n-2) + f(n-3) * e(n-3)
+// where e(n) means the existance of n in the array.
+fn find_route(s: &[usize], i: usize) -> usize {
+    if !s.contains(&i) {
+        return 0;
+    }
+    let route = match i {
+        0 => 1,
+        1 => s.contains(&1) as usize,
+        2 => find_route(&s, 1) + 1,
+        _ => find_route(&s, i - 1) + find_route(&s, i - 2) + find_route(&s, i - 3),
+    };
+    println!("route for {} is {}", i, route);
+    route
 }
 
-fn question1(mut s: Vec<usize>) -> Result<[usize; 3], &'static str> {
+fn question2(s: Vec<usize>) -> Result<usize, &'static str> {
+    Ok(find_route(&s, s[s.len() - 1]))
+}
+
+fn question1(s: Vec<usize>) -> Result<[usize; 3], &'static str> {
     let mut jolts: [usize; 3] = [0; 3];
 
-    s.sort();
     //println!("sorted queue is {:#?}", s);
     jolts[s[0] - 1] += 1;
     for i in 1..s.len() {
@@ -20,7 +35,8 @@ use aoc2020::*;
 fn main() -> Result<(), Box<dyn Error>> {
     let data = load_file()?;
     //println!("{:#?}", data);
-    let data: Vec<usize> = data.iter().map(|s| s.parse::<usize>().unwrap()).collect();
+    let mut data: Vec<usize> = data.iter().map(|s| s.parse::<usize>().unwrap()).collect();
+    data.sort();
     match question1(data.to_owned()) {
         Ok(jolts) => {
             println!("There are {} 1-jolt difference, {} 2-jolts differences and {} 3-jolts differences, and the result of question 1 is {}",jolts[0], jolts[1], jolts[2], jolts[0]*jolts[2]);
@@ -28,8 +44,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         Err(x) => println!("Error processing the input data: {:?}", x),
     };
     match question2(data) {
-        Ok(jolts) => {
-            println!("The sequency from position {:#?}", jolts);
+        Ok(routes) => {
+            println!("There are {} routes", routes);
         }
         Err(x) => println!("Error processing the input data: {:?}", x),
     };
