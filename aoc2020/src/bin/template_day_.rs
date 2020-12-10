07bb3cf03b17855f6d1b04e966594a91dc042d68
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-10 08:08:16 +0000

    template: insert calling to question 1 and 2 to template

--- aoc2020/src/bin/template_day_.rs
+++ aoc2020/src/bin/template_day_.rs
@@ -1,9 +1,27 @@
 use std::error::Error;
 
+fn question2() -> Result<usize, &'static str> {
+    Err("Cannot find the window to fit target number.")
+}
+
+fn question1() -> Result<usize, &'static str> {
+    Err("Cannot find first number.")
+}
 use aoc2020::*;
 fn main() -> Result<(), Box<dyn Error>> {
     let data = load_file()?;
     println!("{:#?}", data);
-
+    match question1() {
+        Ok(x) => {
+            println!("The result for question 1 is {}", x);
+        }
+        Err(x) => println!("Error processing the input data: {:?}", x),
+    };
+    match question2() {
+        Ok(x) => {
+            println!("The sequency from position {}", x);
+        }
+        Err(x) => println!("Error processing the input data: {:?}", x),
+    };
     Ok(())
 }
