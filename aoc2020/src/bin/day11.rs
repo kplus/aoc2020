e8a3dac22fa7638ce910aa6846fdddc940b2c3cc
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-11 09:14:07 +0000

    day11: initial

---
+++ aoc2020/src/bin/day11.rs
@@ -0,0 +1,56 @@
+use std::error::Error;
+
+fn question2() -> Result<usize, &'static str> {
+    Err("Cannot find second number.")
+}
+
+fn question1() -> Result<usize, &'static str> {
+    Err("Cannot find first number.")
+}
+use aoc2020::*;
+fn main() -> Result<(), Box<dyn Error>> {
+    let data = load_file()?;
+    println!("{:#?}", data);
+    match question1() {
+        Ok(x) => {
+            println!("The result for question 1 is {}", x);
+        }
+        Err(x) => eprintln!("Error processing the input data: {:?}", x),
+    };
+    match question2() {
+        Ok(x) => {
+            println!("The sequency from position {}", x);
+        }
+        Err(x) => eprintln!("Error processing the input data: {:?}", x),
+    };
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static TEST_INPUT: &str = r"#.##.##.##
+    #######.##
+    #.#.#..#..
+    ####.##.##
+    #.##.##.##
+    #.#####.##
+    ..#.#.....
+    ##########
+    #.######.#
+    #.#####.##";
+
+    #[test]
+    fn test_question1() {
+        let data: Vec<String> = TEST_INPUT.lines().map(|s| s.trim().to_owned()).collect();
+
+        assert_eq!(Err("Cannot find first number."), question1());
+    }
+    #[test]
+    fn test_question2() {
+        let data: Vec<String> = TEST_INPUT.lines().map(|s| s.trim().to_owned()).collect();
+
+        assert_eq!(Err("Cannot find second number."), question2());
+    }
+}
