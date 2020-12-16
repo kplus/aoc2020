017e8f08a172f257c96a9ef7cddb5c41d0236809
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-16 10:52:31 +0000

    day16: question1: add RULE structure

--- aoc2020/src/bin/day16.rs
+++ aoc2020/src/bin/day16.rs
@@ -2,6 +2,11 @@ use std::error::Error;
 
 use aoc2020::*;
 
+struct RULE {
+    filed: String,               // string of filed name
+    ranges: Vec<(usize, usize)>, // list of valid ranges
+}
+
 fn question2(data: Vec<String>) -> Result<usize, &'static str> {
     Err("Cannot find second number.")
 }
@@ -28,13 +33,24 @@ fn main() -> Result<(), Box<dyn Error>> {
 mod tests {
     use super::*;
 
-    static TEST_INPUT: &str = r"";
+    static TEST_INPUT: &str = r"class: 1-3 or 5-7
+    row: 6-11 or 33-44
+    seat: 13-40 or 45-50
+    
+    your ticket:
+    7,1,14
+    
+    nearby tickets:
+    7,3,47
+    40,4,50
+    55,2,20
+    38,6,12";
 
     #[test]
     fn test_question1() {
         let data: Vec<String> = TEST_INPUT.lines().map(|s| s.trim().to_owned()).collect();
 
-        assert_eq!(Err("Cannot find first number."), question1(data));
+        assert_eq!(Ok(71), question1(data));
     }
     #[test]
     fn test_question2() {
