1890148db1d3fdc8690977a0a80ed2592ed6da7d
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-18 20:03:19 +0000

    day18: refactor to make it compatible with both questions

--- aoc2020/src/bin/day18.rs
+++ aoc2020/src/bin/day18.rs
@@ -8,7 +8,12 @@ enum SIGN {
     Multip,
 }
 
-fn question2(data: &[char], pos: &mut usize, from_multip: bool) -> Result<usize, &'static str> {
+fn question(
+    data: &[char],
+    pos: &mut usize,
+    from_multip: bool,
+    q: usize,
+) -> Result<usize, &'static str> {
     let mut result = 0;
     let mut sign = SIGN::Plus;
     while *pos < data.len() {
@@ -18,26 +23,27 @@ fn question2(data: &[char], pos: &mut usize, from_multip: bool) -> Result<usize,
             '+' => sign = SIGN::Plus,
             '*' => {
                 sign = SIGN::Multip;
-                result *= question2(&data, pos, true).unwrap();
+                if q == 2 {
+                    // multiplication has lower precedence in q2, so we call recursivly to proceed
+                    result *= question(&data, pos, true, q).unwrap();
+                }
             }
-            '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => match sign {
-                SIGN::Multip => {
-                    result *= c.to_digit(10).unwrap() as usize;
-                }
-                SIGN::Plus => {
-                    result += c.to_digit(10).unwrap() as usize;
-                }
-            },
+            x if x.is_digit(10) => {
+                // as there is only single digits input, we can do this
+                let d = c.to_digit(10).unwrap() as usize;
+                match sign {
+                    SIGN::Multip => result *= d,
+                    SIGN::Plus => result += d,
+                };
+            }
             '(' => match sign {
-                SIGN::Multip => {
-                    result *= question2(&data, pos, false).unwrap();
-                }
-                SIGN::Plus => {
-                    result += question2(&data, pos, false).unwrap();
-                }
+                SIGN::Multip => result *= question(&data, pos, false, q).unwrap(),
+                SIGN::Plus => result += question(&data, pos, false, q).unwrap(),
             },
             ')' => {
-                if from_multip {
+                if from_multip && q == 2 {
+                    // multiplication will only return either hits close bracket or end
+                    // if it hits close bracket, it has to return twice
                     *pos -= 1;
                 }
                 return Ok(result);
@@ -48,36 +54,6 @@ fn question2(data: &[char], pos: &mut usize, from_multip: bool) -> Result<usize,
             }
         }
     }
-    //println!("Got a resutl of {}", result);
-    Ok(result)
-}
-fn question1(data: &[char], pos: &mut usize) -> Result<usize, &'static str> {
-    let mut result = 0;
-    let mut sign = SIGN::Plus;
-    while *pos < data.len() {
-        let c = data[*pos];
-        *pos += 1;
-        match c {
-            '+' => sign = SIGN::Plus,
-            '*' => sign = SIGN::Multip,
-            '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => match sign {
-                SIGN::Multip => result *= c.to_digit(10).unwrap() as usize,
-                SIGN::Plus => result += c.to_digit(10).unwrap() as usize,
-            },
-            '(' => match sign {
-                SIGN::Multip => result *= question1(&data, pos).unwrap(),
-                SIGN::Plus => result += question1(&data, pos).unwrap(),
-            },
-            ')' => {
-                return Ok(result);
-            }
-            ' ' => {}
-            _ => {
-                return Err("Unexpected character in expression.");
-            }
-        }
-    }
-    //println!("Got a resutl of {}", result);
     Ok(result)
 }
 
@@ -85,27 +61,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     let data = load_file()?;
     //println!("{:#?}", data);
 
-    let mut sum = 0;
-    for exp in data.to_owned() {
-        let mut pos = 0;
-        let exp: Vec<char> = exp.chars().collect();
-        match question1(&exp, &mut pos) {
-            Ok(x) => sum += x,
-            Err(x) => eprintln!("Error processing the input data: {:?}", x),
-        };
-    }
-    println!("The sum for question 1 {}", sum);
-
-    let mut sum = 0;
+    let mut sum = [0, 0];
     for exp in data {
-        let mut pos = 0;
+        let mut pos = [0, 0];
         let exp: Vec<char> = exp.chars().collect();
-        match question2(&exp, &mut pos, false) {
-            Ok(x) => sum += x,
-            Err(x) => eprintln!("Error processing the input data: {:?}", x),
-        };
+        for i in 0..2 {
+            match question(&exp, &mut pos[i], false, i + 1) {
+                Ok(x) => sum[i] += x,
+                Err(x) => eprintln!("Error processing the input data: {:?}", x),
+            };
+        }
     }
-    println!("The sum for question 2 {}", sum);
+    println!("The sum for question 1 {}", sum[0]);
+    println!("The sum for question 2 {}", sum[1]);
     Ok(())
 }
 
@@ -124,23 +92,21 @@ mod tests {
         let data: Vec<String> = TEST_INPUT.lines().map(|s| s.trim().to_owned()).collect();
         let data_exps: Vec<Vec<char>> = data.iter().map(|line| line.chars().collect()).collect();
 
-        assert_eq!(Ok(71), question1(&data_exps[0], &mut 0));
-        assert_eq!(Ok(26), question1(&data_exps[1], &mut 0));
-        assert_eq!(Ok(437), question1(&data_exps[2], &mut 0));
-        assert_eq!(Ok(12240), question1(&data_exps[3], &mut 0));
-        assert_eq!(Ok(13632), question1(&data_exps[4], &mut 0));
+        assert_eq!(Ok(71), question(&data_exps[0], &mut 0, false, 1));
+        assert_eq!(Ok(26), question(&data_exps[1], &mut 0, false, 1));
+        assert_eq!(Ok(437), question(&data_exps[2], &mut 0, false, 1));
+        assert_eq!(Ok(12240), question(&data_exps[3], &mut 0, false, 1));
+        assert_eq!(Ok(13632), question(&data_exps[4], &mut 0, false, 1));
     }
     #[test]
     fn test_question2() {
         let data: Vec<String> = TEST_INPUT.lines().map(|s| s.trim().to_owned()).collect();
         let data_exps: Vec<Vec<char>> = data.iter().map(|line| line.chars().collect()).collect();
 
-        println!("input are {:?}", data_exps);
-
-        assert_eq!(Ok(231), question2(&data_exps[0], &mut 0, false));
-        assert_eq!(Ok(46), question2(&data_exps[1], &mut 0, false));
-        assert_eq!(Ok(1445), question2(&data_exps[2], &mut 0, false));
-        assert_eq!(Ok(669060), question2(&data_exps[3], &mut 0, false));
-        assert_eq!(Ok(23340), question2(&data_exps[4], &mut 0, false));
+        assert_eq!(Ok(231), question(&data_exps[0], &mut 0, false, 2));
+        assert_eq!(Ok(46), question(&data_exps[1], &mut 0, false, 2));
+        assert_eq!(Ok(1445), question(&data_exps[2], &mut 0, false, 2));
+        assert_eq!(Ok(669060), question(&data_exps[3], &mut 0, false, 2));
+        assert_eq!(Ok(23340), question(&data_exps[4], &mut 0, false, 2));
     }
 }
