c3b5b8bca54c07ea1e0079f3305186f4db0e68b8
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-18 10:43:20 +0000

    day18: question2: done

--- aoc2020/src/bin/day18.rs
+++ aoc2020/src/bin/day18.rs
@@ -2,25 +2,61 @@ use std::error::Error;
 
 use aoc2020::*;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum SIGN {
     Plus,
     Multip,
 }
-fn question2(data: Vec<String>) -> Result<usize, &'static str> {
-    Err("Cannot find second number.")
-}
 
-fn question1(data: &Vec<char>, pos: &mut usize) -> Result<usize, &'static str> {
+fn question2(data: &[char], pos: &mut usize, from_multip: bool) -> Result<usize, &'static str> {
     let mut result = 0;
     let mut sign = SIGN::Plus;
     while *pos < data.len() {
         let c = data[*pos];
         *pos += 1;
-        println!(
-            "Current char is {}, at position {}, sign is {:?}, result is {}",
-            c, pos, sign, result
-        );
+        match c {
+            '+' => sign = SIGN::Plus,
+            '*' => {
+                sign = SIGN::Multip;
+                result *= question2(&data, pos, true).unwrap();
+            }
+            '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => match sign {
+                SIGN::Multip => {
+                    result *= c.to_digit(10).unwrap() as usize;
+                }
+                SIGN::Plus => {
+                    result += c.to_digit(10).unwrap() as usize;
+                }
+            },
+            '(' => match sign {
+                SIGN::Multip => {
+                    result *= question2(&data, pos, false).unwrap();
+                }
+                SIGN::Plus => {
+                    result += question2(&data, pos, false).unwrap();
+                }
+            },
+            ')' => {
+                if from_multip {
+                    *pos -= 1;
+                }
+                return Ok(result);
+            }
+            ' ' => {}
+            _ => {
+                return Err("Unexpected character in expression.");
+            }
+        }
+    }
+    //println!("Got a resutl of {}", result);
+    Ok(result)
+}
+fn question1(data: &[char], pos: &mut usize) -> Result<usize, &'static str> {
+    let mut result = 0;
+    let mut sign = SIGN::Plus;
+    while *pos < data.len() {
+        let c = data[*pos];
+        *pos += 1;
         match c {
             '+' => sign = SIGN::Plus,
             '*' => sign = SIGN::Multip,
@@ -41,7 +77,7 @@ fn question1(data: &Vec<char>, pos: &mut usize) -> Result<usize, &'static str> {
             }
         }
     }
-    println!("Got a resutl of {}", result);
+    //println!("Got a resutl of {}", result);
     Ok(result)
 }
 
@@ -50,7 +86,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     //println!("{:#?}", data);
 
     let mut sum = 0;
-    for exp in data {
+    for exp in data.to_owned() {
         let mut pos = 0;
         let exp: Vec<char> = exp.chars().collect();
         match question1(&exp, &mut pos) {
@@ -58,14 +94,18 @@ fn main() -> Result<(), Box<dyn Error>> {
             Err(x) => eprintln!("Error processing the input data: {:?}", x),
         };
     }
+    println!("The sum for question 1 {}", sum);
 
-    println!("The sequency from position {}", sum);
-    /*
-    match question2(data) {
-        Ok(x) => println!("The sequency from position {}", x),
-        Err(x) => eprintln!("Error processing the input data: {:?}", x),
-    };
-    */
+    let mut sum = 0;
+    for exp in data {
+        let mut pos = 0;
+        let exp: Vec<char> = exp.chars().collect();
+        match question2(&exp, &mut pos, false) {
+            Ok(x) => sum += x,
+            Err(x) => eprintln!("Error processing the input data: {:?}", x),
+        };
+    }
+    println!("The sum for question 2 {}", sum);
     Ok(())
 }
 
@@ -82,17 +122,25 @@ mod tests {
     #[test]
     fn test_question1() {
         let data: Vec<String> = TEST_INPUT.lines().map(|s| s.trim().to_owned()).collect();
+        let data_exps: Vec<Vec<char>> = data.iter().map(|line| line.chars().collect()).collect();
 
-        assert_eq!(Ok(71), question1(&data[0].chars().collect(), &mut 0));
-        assert_eq!(Ok(26), question1(&data[1].chars().collect(), &mut 0));
-        assert_eq!(Ok(437), question1(&data[2].chars().collect(), &mut 0));
-        assert_eq!(Ok(12240), question1(&data[3].chars().collect(), &mut 0));
-        assert_eq!(Ok(13632), question1(&data[4].chars().collect(), &mut 0));
+        assert_eq!(Ok(71), question1(&data_exps[0], &mut 0));
+        assert_eq!(Ok(26), question1(&data_exps[1], &mut 0));
+        assert_eq!(Ok(437), question1(&data_exps[2], &mut 0));
+        assert_eq!(Ok(12240), question1(&data_exps[3], &mut 0));
+        assert_eq!(Ok(13632), question1(&data_exps[4], &mut 0));
     }
     #[test]
     fn test_question2() {
         let data: Vec<String> = TEST_INPUT.lines().map(|s| s.trim().to_owned()).collect();
+        let data_exps: Vec<Vec<char>> = data.iter().map(|line| line.chars().collect()).collect();
 
-        assert_eq!(Err("Cannot find second number."), question2(data));
+        println!("input are {:?}", data_exps);
+
+        assert_eq!(Ok(231), question2(&data_exps[0], &mut 0, false));
+        assert_eq!(Ok(46), question2(&data_exps[1], &mut 0, false));
+        assert_eq!(Ok(1445), question2(&data_exps[2], &mut 0, false));
+        assert_eq!(Ok(669060), question2(&data_exps[3], &mut 0, false));
+        assert_eq!(Ok(23340), question2(&data_exps[4], &mut 0, false));
     }
 }
