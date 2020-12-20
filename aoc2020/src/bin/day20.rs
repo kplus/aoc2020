4707ab9d1dca1bc702a3cdf2939fff6721b68d1a
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-20 21:40:44 +0000

    day20: question1: wip: sometimes work
    willl trigger error sometimes, but re run sometimes
    makes it work... really weird

--- aoc2020/src/bin/day20.rs
+++ aoc2020/src/bin/day20.rs
@@ -7,7 +7,7 @@ use integer_sqrt::IntegerSquareRoot;
 
 use aoc2020::*;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 enum BorderDirection {
     LEFT,
     RIGHT,
@@ -18,18 +18,14 @@ enum BorderDirection {
 #[derive(PartialEq, Eq, Hash, Debug, Clone)]
 struct TILE {
     id: usize,
-    //x: isize,
-    //y: isize,
-    left: String,
-    right: String,
-    top: String,
-    bottum: String,
+    horizon: Vec<String>,
+    vertical: Vec<String>,
 }
 
 impl TILE {
     // Create TILE from block strings
     fn from_str(s: String) -> Self {
-        //println!("create tile from string {:?}", s);
+        println!("create tile from string {:?}", s);
         let v: Vec<&str> = s.lines().map(|s| s.trim()).collect();
 
         let id = v[0]
@@ -39,6 +35,8 @@ impl TILE {
             .parse::<usize>()
             .unwrap();
         let grade = v.len() - 1;
+        let mut horizon = Vec::new();
+        let mut vertical = Vec::new();
         let top = v[1].to_string();
         let bottum = v[grade].to_string();
         let mut left = String::new();
@@ -48,65 +46,99 @@ impl TILE {
             left.push(c[0]);
             right.push(c[grade - 1]);
         }
+        horizon.push(left);
+        horizon.push(right);
+        vertical.push(top);
+        vertical.push(bottum);
         TILE {
             id,
-            left,
-            right,
-            top,
-            bottum,
+            horizon,
+            vertical,
         }
     }
+    /* seems we don't really need these method at moment, as we supply the right front line directly
+        //todo: Rotate TILE by multiple of 90 degrees
+        fn rotate(&mut self, angle: usize) {}
 
-    //todo: Rotate TILE by multiple of 90 degrees
-    fn rotate(&mut self, angle: usize) {}
-
-    //todo: Flip TILE in horizon direction or vertical direction
-    fn flip(&mut self, horizon: bool) {}
-
-    fn get_id(&self) -> usize {
+        //todo: Flip TILE in horizon direction or vertical direction
+        fn flip(&mut self, horizon: bool) {}
+    */
+    fn tile_id(&self) -> usize {
         self.id
     }
-    fn get_border(&self, border: &BorderDirection) -> String {
+    // we don't really care about which border is it, as long as it's on same direction
+    fn get_border(&mut self, border: &BorderDirection) -> String {
         match border {
-            BorderDirection::LEFT => self.left.to_owned(),
-            BorderDirection::RIGHT => self.right.to_owned(),
-            BorderDirection::TOP => self.top.to_owned(),
-            BorderDirection::BOTTUM => self.bottum.to_owned(),
+            BorderDirection::LEFT | BorderDirection::RIGHT => {
+                let ret = self.horizon[0].to_owned();
+                self.horizon.remove(0);
+                ret
+            }
+            BorderDirection::TOP | BorderDirection::BOTTUM => {
+                if !self.vertical.is_empty() {
+                    let ret = self.vertical[0].to_owned();
+                    self.vertical.remove(0);
+                    ret
+                } else {
+                    let ret = self.horizon[0].to_owned();
+                    self.horizon.remove(0);
+                    ret
+                }
+            }
         }
     }
 
-    //todo: Check if current tile lines up with the front
-    fn line_up(&self, s: &str, b: &BorderDirection) -> Option<String> {
-        if *s == self.bottum {
-            return Some(self.top.to_owned());
-        } else if *s == self.top {
-            return Some(self.bottum.to_owned());
-        } else if *s == self.left {
-            return Some(self.right.to_owned());
-        } else if *s == self.right {
-            return Some(self.left.to_owned());
+    // Check if current tile lines up with the front
+    // Return opposite line if lined up or none if not line up
+    fn line_up(&mut self, s: &String) -> Option<String> {
+        println!("checking string {} with tile {}", s, self.id);
+        let mut ret = String::new();
+        if self.horizon.contains(s) {
+            if self.horizon[0] == *s {
+                ret.push_str(self.horizon[1].as_str());
+            } else {
+                ret.push_str(self.horizon[0].as_str());
+            }
+            self.horizon.clear();
+            return Some(ret);
+        } else if self.vertical.contains(s) {
+            if self.vertical[0] == *s {
+                ret.push_str(self.vertical[1].as_str());
+            } else {
+                ret.push_str(self.vertical[0].as_str());
+            }
+            self.vertical.clear();
+            return Some(ret);
         }
 
         let r: String = s.chars().rev().collect();
-
-        if r == self.bottum {
-            return Some(self.top.chars().rev().collect::<String>());
-        } else if *s == self.top {
-            return Some(self.bottum.chars().rev().collect::<String>());
-        } else if *s == self.left {
-            return Some(self.right.chars().rev().collect::<String>());
-        } else if *s == self.right {
-            return Some(self.left.chars().rev().collect::<String>());
+        println!("checking reversed string {} with tile {}", r, self.id);
+        if self.horizon.contains(&r) {
+            if self.horizon[0] == *r {
+                ret = self.horizon[1].to_owned();
+            } else {
+                ret = self.horizon[0].to_owned();
+            }
+            self.horizon.clear();
+            return Some(ret);
+        } else if self.vertical.contains(&r) {
+            if self.vertical[0] == *r {
+                ret = self.vertical[1].to_owned();
+            } else {
+                ret = self.vertical[0].to_owned();
+            }
+            self.vertical.clear();
+            return Some(ret);
         }
         None
     }
 }
 
+#[derive(Debug)]
 struct IMAGE {
-    front: String,                      // the border string to check
-    x_shift: isize,                     // image coordinate origin related to first tile in x-axis
-    y_shift: isize,                     // image coordinate origin related to first tile in y-axis
-    grade: usize,                       // grade of image
+    x_shift: isize, // image coordinate origin related to first tile in x-axis
+    y_shift: isize, // image coordinate origin related to first tile in y-axis
+    grade: usize,   // grade of image
     map: HashMap<(isize, isize), TILE>, // map for confirmed TILEs
 }
 
@@ -114,7 +146,6 @@ impl IMAGE {
     //todo: Initialization of Image
     fn new(grade: usize) -> Self {
         IMAGE {
-            front: String::from(""),
             x_shift: 0,
             y_shift: 0,
             grade,
@@ -125,34 +156,44 @@ impl IMAGE {
     fn get_end(&self, b: &BorderDirection) -> isize {
         match b {
             BorderDirection::LEFT => self.x_shift,
-            BorderDirection::RIGHT => self.grade as isize + self.x_shift,
-            BorderDirection::TOP => self.grade as isize + self.y_shift,
+            BorderDirection::RIGHT => self.grade as isize + self.x_shift - 1,
+            BorderDirection::TOP => self.grade as isize + self.y_shift - 1,
             BorderDirection::BOTTUM => self.y_shift,
         }
     }
     fn set_range(&mut self, end: isize, b: &BorderDirection) {
         match b {
-            BorderDirection::LEFT | BorderDirection::RIGHT => {
-                self.x_shift = end + 1 - self.grade as isize
-            }
-            BorderDirection::TOP | BorderDirection::BOTTUM => {
-                self.y_shift = end + 1 - self.grade as isize
-            }
+            BorderDirection::RIGHT => self.x_shift = end + 1 - self.grade as isize,
+            BorderDirection::TOP => self.y_shift = end + 1 - self.grade as isize,
+            _ => {}
         };
+        //println!(
+        //    "passed in end is {}, x shift set to {}, y shift set to {}",
+        //    end, self.x_shift, self.y_shift
+        //);
     }
     fn get_id(&self, x: isize, y: isize) -> usize {
-        self.map.get(&(x, y)).unwrap().get_id()
+        println!("x is {}, y is {}", x, y);
+        self.map.get(&(x, y)).unwrap().tile_id()
     }
-    fn get_front(&self, x: isize, b: &BorderDirection) -> String {
-        self.map.get(&(x, 0)).unwrap().get_border(b)
+    fn get_front(&mut self, x: isize, b: &BorderDirection) -> String {
+        if self.map.is_empty() {
+            return String::from("");
+        }
+        //println!("try to get border line {:?} for {}-0", b, x);
+        self.map.get_mut(&(x, 0)).unwrap().get_border(b)
     }
 
-    fn store(&mut self, tile: TILE, x: isize, y: isize, b: &BorderDirection) {
+    fn store(&mut self, tile: TILE, x: isize, i: isize, b: &BorderDirection) {
         if *b == BorderDirection::LEFT || *b == BorderDirection::RIGHT {
-            self.map.insert((x, 0), tile);
+            self.map.insert((i, 0), tile);
         } else {
-            self.map.insert((x, y), tile);
+            self.map.insert((x, i), tile);
         }
+    }
+
+    fn map_is_empty(&self) -> bool {
+        self.map.is_empty()
     }
 }
 fn question2(data: Vec<String>) -> Result<usize, &'static str> {
@@ -169,23 +210,54 @@ fn fill_one_direction(
     x: isize,
 ) {
     let end = image.get_end(&direction);
+    //println!("end is {}, x is {}", end, x);
     let mut front = image.get_front(x, &direction);
-    for i in 0..end {
+    //println!("front is {:?}", front);
+
+    for i in 0..=end.abs() {
+        let mut index = i;
+        if end < 0 {
+            index *= -1;
+        }
+        //println!("now at postion {}, front is {:?}", index, front);
+        let mut added_new_tile = false;
         for tile in tiles_pool.iter().cloned() {
-            match tile.line_up(&front, &direction) {
+            if image.map_is_empty() {
+                // println!("put first tile in");
+                image.store(tile.to_owned(), 0, 0, &direction);
+                added_new_tile = true;
+                front = image.get_front(0, &direction);
+                tiles_pool.remove(&tile);
+                break;
+            }
+            if i == 0 {
+                added_new_tile = true;
+                break;
+            }
+            let mut tile_change = tile.to_owned();
+            match tile_change.line_up(&front) {
                 Some(f) => {
+                    //println!(
+                    //    "line up found, update front to {:?}, tile stored to {} - {}",
+                    //    f, x, index
+                    // );
                     front = f;
-                    image.store(tile.to_owned(), x, i, &direction);
+                    image.store(tile_change, x, index, &direction);
+                    added_new_tile = true;
                     tiles_pool.remove(&tile);
+                    // println!("i is {} at end of filling cycle", index);
                     break;
                 } // break into next position in the image
                 None => {
-                    continue;
+                    //println!("no line up found, move to next tile");
                 }
             }
         }
-        // not line up found, it hits the edge
-        image.set_range(i, &direction);
+        if !added_new_tile {
+            image.set_range(index - 1, &direction);
+            break;
+        }
+        image.set_range(index, &direction);
     }
 }
 
@@ -195,11 +267,11 @@ fn question1(data: Vec<String>) -> Result<usize, &'static str> {
     for s in data {
         tiles_pool.insert(TILE::from_str(s));
     }
-    //println!(
-    //    "the tiles pool is {:#?}, the length is {}",
-    //    tiles_pool,
-    //    tiles_pool.len()
-    //);
+    println!(
+        "the tiles pool is {:#?}, the length is {}",
+        tiles_pool,
+        tiles_pool.len()
+    );
 
     let grade = tiles_pool.len().integer_sqrt();
     //println!("grade is {}", grade);
@@ -207,12 +279,15 @@ fn question1(data: Vec<String>) -> Result<usize, &'static str> {
 
     fill_one_direction(&mut tiles_pool, &mut image, BorderDirection::RIGHT, 0);
     let x_start = image.get_end(&BorderDirection::LEFT);
+    //println!("x start is {}", x_start);
     if x_start < 0 {
         // if there is a shift
         fill_one_direction(&mut tiles_pool, &mut image, BorderDirection::LEFT, 0);
     }
-    let x_end = grade as isize + x_start;
-    for i in x_start..x_end {
+    let x_end = grade as isize + x_start - 1;
+    println!("x start is {}, end is {}", x_start, x_end);
+    for i in x_start..=x_end {
+        println!("start filling colum {}", i);
         fill_one_direction(&mut tiles_pool, &mut image, BorderDirection::TOP, i);
         let y_start = image.get_end(&BorderDirection::BOTTUM);
         if y_start < 0 {
@@ -221,8 +296,9 @@ fn question1(data: Vec<String>) -> Result<usize, &'static str> {
         }
     }
 
+    println!("The tile pool is {:#?}, image is {:#?}", tiles_pool, image);
     let y_start = image.get_end(&BorderDirection::BOTTUM);
-    let y_end = grade as isize + y_start;
+    let y_end = grade as isize + y_start - 1;
     println!(
         "image range is from x {} - {}, y {} - {}",
         x_start, x_end, y_start, y_end
