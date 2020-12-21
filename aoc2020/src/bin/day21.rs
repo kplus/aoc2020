f7b1ebdf5a1104bdc1f240d7c97bee7b499ded5d
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-21 17:07:46 +0000

    day21: question1: replace u128 mask with structure Mask

--- aoc2020/src/bin/day21.rs
+++ aoc2020/src/bin/day21.rs
@@ -1,7 +1,102 @@
 use aoc2020::*;
 use std::collections::HashMap;
 use std::error::Error;
+use std::ops::*;
 
+#[derive(Debug, Clone)]
+struct Mask {
+    masks: Vec<u128>,
+}
+impl Mask {
+    fn new() -> Self {
+        Self { masks: Vec::new() }
+    }
+
+    fn max() -> Self {
+        const MAX_GROUP: usize = 10; // set maximum mask vector to be 10 for now
+
+        Self {
+            masks: vec![u128::MAX; MAX_GROUP],
+        }
+    }
+    fn count_ones(&self) -> u32 {
+        let mut ones = 0;
+        for mask in &self.masks {
+            ones += mask.count_ones();
+        }
+        ones
+    }
+}
+impl BitOrAssign for Mask {
+    fn bitor_assign(&mut self, rhs: Self) {
+        let count = self.masks.len();
+        for i in 0..rhs.masks.len() {
+            if i <= count {
+                self.masks[i] |= rhs.masks[i];
+            } else {
+                self.masks.push(rhs.masks[i]);
+            }
+        }
+    }
+}
+impl Not for Mask {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        let mut out = Vec::new();
+        for mask in self.masks {
+            out.push(!mask);
+        }
+        Self { masks: out }
+    }
+}
+impl BitAnd for Mask {
+    type Output = Self;
+
+    // rhs is the "right-hand side" of the expression `a & b`
+    fn bitand(self, rhs: Self) -> Self::Output {
+        let mut out = Vec::new();
+        let count = self.masks.len();
+        out.copy_from_slice(&self.masks);
+        for (i, mask) in rhs.masks.iter().enumerate() {
+            if i <= count {
+                out[i] &= mask;
+            }
+        }
+        Self { masks: out }
+    }
+}
+impl BitAndAssign for Mask {
+    // rhs is the "right-hand side" of the expression `a &= b`
+    fn bitand_assign(&mut self, rhs: Self) {
+        let count = self.masks.len();
+        for i in 0..rhs.masks.len() {
+            if i <= count {
+                self.masks[i] &= rhs.masks[i];
+            }
+        }
+    }
+}
+/*
+impl BitOr for Mask {
+    type Output = Self;
+
+    // rhs is the "right-hand side" of the expression `a | b`
+    fn bitor(self, rhs: Self) -> Self::Output {
+        let mut out = Vec::new();
+        let count = self.masks.len();
+        out.copy_from_slice(&self.masks);
+        for i in 0..rhs.masks.len() {
+            if i <= count {
+                out[i] |= rhs.masks[i];
+            } else {
+                out.push(rhs.masks[i]);
+            }
+        }
+        Self { masks: out }
+    }
+}
+*/
 #[derive(Debug)]
 struct MaskLookUp {
     count: usize,
@@ -23,13 +118,21 @@ impl MaskLookUp {
     fn insert(&mut self, s: &str) {
         self.mask_table.insert(s.to_owned(), self.count);
         self.count += 1;
-        if self.count == 128 {
-            panic!("Item count exceeds 128 can hold, consider to increase mask length");
+        if self.count == 1280 {
+            panic!("Item count exceeds 1280, consider to increase MAX const in Mask");
         };
     }
 
-    fn get_mask(&self, s: &str) -> usize {
-        *self.mask_table.get(s).unwrap()
+    fn get_mask(&self, s: &str) -> Mask {
+        let index = self.mask_table.get(s).unwrap();
+        let group = index / 128;
+        let offset = index % 128;
+        let mut out = Vec::new();
+        for _i in 0..group {
+            out.push(0);
+        }
+        out.push(1 << offset);
+        Mask { masks: out }
     }
 }
 
@@ -37,17 +140,17 @@ fn question2(data: Vec<String>) -> Result<usize, &'static str> {
     Err("Cannot find second number.")
 }
 
-//todo: Parse the input line, and fill in the tables we need
+// Parse the input line, and fill in the tables we need
 fn parse(
     s: String,
     mask_table: &mut MaskLookUp,
-    allergens: &mut HashMap<String, u128>,
-    string_masks: &mut Vec<u128>,
+    allergens: &mut HashMap<String, Mask>,
+    string_masks: &mut Vec<Mask>,
 ) {
     println!("String to parse is {}", s);
 
     let tmp_s: Vec<&str> = s.split_terminator("(contains").map(|s| s.trim()).collect();
-    let mut string_mask = 0;
+    let mut string_mask = Mask::new();
 
     // go through the ingredients for this food, update mask lookup table if needed
     // and cache the mask of current string
@@ -57,9 +160,9 @@ fn parse(
             println!("doesn't exist, so insert it");
             mask_table.insert(ingredient);
         }
-        string_mask |= 1 << mask_table.get_mask(ingredient);
+        string_mask |= mask_table.get_mask(ingredient);
     }
-    println!("got string mask as {}", string_mask);
+    println!("got string mask as {:#?}", string_mask);
     string_masks.push(string_mask.to_owned());
 
     // go through the allergens in this food, update allergens table
@@ -67,8 +170,10 @@ fn parse(
         .split_terminator(|c: char| c.is_ascii_punctuation())
         .map(|s| s.trim())
     {
-        let mask = allergens.entry(allergen.to_owned()).or_insert(u128::MAX);
-        *mask &= string_mask;
+        let mask = allergens
+            .entry(allergen.to_owned())
+            .or_insert_with(Mask::max);
+        *mask &= string_mask.to_owned();
     }
 }
 
@@ -82,9 +187,9 @@ fn question1(data: Vec<String>) -> Result<usize, &'static str> {
     }
 
     // got the whole mask for all allergens
-    let mut allergen_mask = 0;
+    let mut allergen_mask = Mask::new();
     for v in allergens.values() {
-        allergen_mask |= v;
+        allergen_mask |= v.to_owned();
     }
 
     println!(
@@ -95,7 +200,7 @@ fn question1(data: Vec<String>) -> Result<usize, &'static str> {
     // count the non-possible allergen ones
     let mut count = 0;
     for m in string_masks {
-        count += (m & !allergen_mask).count_ones();
+        count += (m & !allergen_mask.to_owned()).count_ones();
     }
     Ok(count as usize)
 }
