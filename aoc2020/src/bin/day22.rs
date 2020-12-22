0574a34770be5360a4836cc0cec3dc5afdf19493
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-22 19:10:11 +0000

    day22: question2

--- aoc2020/src/bin/day22.rs
+++ aoc2020/src/bin/day22.rs
@@ -2,11 +2,60 @@ use std::error::Error;
 
 use aoc2020::*;
 
+#[derive(PartialEq)]
+enum Winner {
+    Player1,
+    Player2,
+}
+// Run a round to compare and move cards for question 2
+fn run2(p1: &mut Vec<usize>, p2: &mut Vec<usize>) {
+    let mut check_repeat = Vec::new();
+    while !p1.is_empty() && !p2.is_empty() {
+        let p1_head = p1.remove(0);
+        let p2_head = p2.remove(0);
+        let winner;
+        if check_repeat.contains(p1) {
+            break;
+        }
+
+        check_repeat.push(p1.to_owned());
+
+        if p1_head <= p1.len() && p2_head <= p2.len() {
+            //println!("Entering sub game");
+            let mut p1_sub = p1[0..p1_head].to_owned();
+            let mut p2_sub = p2[0..p2_head].to_owned();
+            run2(&mut p1_sub, &mut p2_sub);
+
+            if p1_sub.is_empty() {
+                winner = Winner::Player2;
+            } else {
+                winner = Winner::Player1;
+            }
+        } else if p1_head > p2_head {
+            winner = Winner::Player1;
+        } else {
+            winner = Winner::Player2;
+        }
+
+        if winner == Winner::Player1 {
+            p1.push(p1_head);
+            p1.push(p2_head);
+        } else {
+            p2.push(p2_head);
+            p2.push(p1_head);
+        }
+        //println!("stacks are:\n p1: {:?}\n p2: {:?}", p1, p2);
+    }
+}
 fn question2(data: Vec<String>) -> Result<usize, &'static str> {
-    Err("Cannot find second number.")
+    let (mut player1, mut player2) = get_stack(data);
+
+    run2(&mut player1, &mut player2);
+
+    Ok(output(player1, player2))
 }
 
-// Run a round to compare and move cards
+// Run a round to compare and move cards for question 1
 fn run(p1: &mut Vec<usize>, p2: &mut Vec<usize>) {
     let p1_head = p1.remove(0);
     let p2_head = p2.remove(0);
@@ -19,6 +68,7 @@ fn run(p1: &mut Vec<usize>, p2: &mut Vec<usize>) {
     }
     //println!("stacks are:\n p1: {:?}\n p2: {:?}", p1, p2);
 }
+
 fn get_stack(mut player1: Vec<String>) -> (Vec<usize>, Vec<usize>) {
     let len = player1.len();
     let player2 = player1.split_off(len / 2 + 1);
