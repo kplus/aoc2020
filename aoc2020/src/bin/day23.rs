5192f8c6f20d4662d47f6848bccc5f042d1d52ce
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-23 22:40:45 +0000

    day23: refactor to use hashmap based linked list instead of vec

--- aoc2020/src/bin/day23.rs
+++ aoc2020/src/bin/day23.rs
@@ -1,119 +1,114 @@
+use std::collections::HashMap;
 use std::error::Error;
 
+#[derive(Debug)]
 struct LIST {
-    list: Vec<usize>,
+    big: Vec<usize>,             //cache the biggest 4 cup numbers
+    list: HashMap<usize, usize>, //key:value -> cup_number:next_cup
 }
 
 impl LIST {
     fn from_str(s: &str) -> Self {
-        Self {
-            list: s
-                .chars()
-                .map(|c| c.to_digit(10).unwrap() as usize)
-                .collect(),
-        }
-    }
-
-    // try to find a cup in the current LIST
-    // return index if found, or None if not
-    fn find(&self, value: usize) -> Option<usize> {
-        for (i, v) in self.list.iter().enumerate() {
-            if *v == value {
-                return Some(i);
+        const CACHE_BIG: usize = 4;
+        let mut big = vec![0; CACHE_BIG];
+        let mut list = HashMap::new();
+        let mut next: usize = s.chars().next().unwrap().to_digit(10).unwrap() as usize;
+        for i in s.chars().map(|c| c.to_digit(10).unwrap() as usize).rev() {
+            for j in 0..CACHE_BIG {
+                if i > big[j] {
+                    big.insert(j, i);
+                    big.truncate(CACHE_BIG);
+                    break;
+                }
             }
+
+            list.insert(i, next);
+            next = i;
         }
-        None
+
+        Self { big, list }
     }
 
-    //get the nth cup in the current LIST
-    fn get(&self, index: usize) -> usize {
-        self.list[index]
+    //Find the cup for next step, it should be current cup advanced by 3
+    fn step(&self, current: usize, steps: usize) -> usize {
+        let mut next = current;
+        for _i in 0..=steps {
+            next = *self.list.get(&next).unwrap();
+        }
+        next
     }
 
-    // Shift list from index
-    fn shift(&mut self, index: usize) {
-        let mut tmp = self.list.split_off(index);
-        tmp.append(&mut self.list);
-        self.list = tmp;
-    }
+    fn get_move(&mut self, current: usize, num: usize) -> Vec<usize> {
+        let mut to_move = Vec::new();
+        let mut ptr = current;
 
-    fn get_len(&self) -> usize {
-        self.list.len()
-    }
-
-    // Remove n element from right of current cup and return the slice
-    fn remove(&mut self, index: usize, num: usize) -> Vec<usize> {
-        let mut remove = Vec::new();
         for _i in 0..num {
-            remove.push(self.list.remove(index + 1));
+            ptr = *self.list.get(&ptr).unwrap();
+            to_move.push(ptr);
         }
-        remove
+
+        self.list.insert(current, *self.list.get(&ptr).unwrap());
+
+        to_move
     }
 
-    // Insert the Vec into indicated position of LIST
-    fn insert(&mut self, v: Vec<usize>, index: usize) {
-        let mut i = index;
-        for cup in v {
-            i += 1;
-            self.list.insert(i, cup);
-        }
+    // Insert the cups into indicated position of linkedlist
+    fn insert(&mut self, v: Vec<usize>, dest: usize) {
+        //println!("dest is {}", dest);
+        let joint = *self.list.get(&dest).unwrap();
+        self.list.insert(dest, v[0]);
+        self.list.insert(v[v.len() - 1], joint);
     }
 
-    //Return the index of biggest cup in current list
-    fn biggest(&self) -> usize {
-        let mut biggest = 0;
-        let mut index = 0;
-        for (i, v) in self.list.iter().enumerate() {
-            if *v > biggest {
-                biggest = *v;
-                index = i;
+    // Return the index of biggest cup in current list
+    fn biggest(&self, cups_to_move: &[usize]) -> usize {
+        for b in &self.big {
+            if !cups_to_move.contains(&b) {
+                return *b;
             }
         }
-        index
+        0 // this will never happen, cups to move only have 3(big_four has 4)
     }
 
+    //Get the list string from the final LIST
     fn get_list_string(&self) -> String {
-        self.list.iter().map(|i| i.to_string()).collect::<String>()
+        let mut final_list = Vec::new();
+        let mut next = *self.list.get(&1).unwrap();
+        while next != 1 {
+            final_list.push(next);
+            next = *self.list.get(&next).unwrap();
+        }
+        final_list
+            .into_iter()
+            .map(|d| d.to_string())
+            .collect::<String>()
     }
 }
 
 fn question(data: &str) -> Result<String, &'static str> {
     const MOVES: usize = 100;
     let mut list = LIST::from_str(data);
-    let len = list.get_len();
-    let mut next_cup = list.get(0);
-    for _i in 0..MOVES {
-        //doing: Do a move on the list
-        let mut current_index = list.find(next_cup).unwrap();
-        if len - current_index < 4 {
-            //not enough elements to move at the tail of LIST
-            list.shift(current_index);
-            current_index = 0
-        }
+    //println!("initial list is {:#?}", list);
+    let mut next_cup = data.chars().next().unwrap().to_digit(10).unwrap() as usize; // start point
+    for i in 0..MOVES {
+        let current_cup = next_cup;
+        next_cup = list.step(current_cup, 3);
 
-        let mut dest = next_cup - 1;
-        next_cup = {
-            if (len - current_index) == 4 {
-                list.get(0)
-            } else {
-                list.get(current_index + 4)
-            }
-        }; //update and save next cup to deal with
-        let cups_moved = list.remove(current_index, 3);
+        let mut dest = current_cup - 1;
 
-        while cups_moved.contains(&dest) {
+        let cups_to_move = list.get_move(current_cup, 3);
+
+        while cups_to_move.contains(&dest) {
             dest -= 1;
         }
 
-        let dest_index = if dest == 0 {
-            list.biggest()
-        } else {
-            list.find(dest).unwrap()
+        if dest == 0 {
+            dest = list.biggest(&cups_to_move);
         };
-        list.insert(cups_moved, dest_index);
+
+        list.insert(cups_to_move, dest);
     }
-    list.shift(list.find(1).unwrap());
-    //println!("final list is {}", list);
+    //println!("final list is {:?}", list);
     Ok(list.get_list_string())
 }
 
@@ -136,6 +131,6 @@ mod tests {
 
     #[test]
     fn test_question() {
-        assert_eq!(Ok(String::from("167384529")), question1(TEST_INPUT));
+        assert_eq!(Ok(String::from("67384529")), question(TEST_INPUT));
     }
 }
