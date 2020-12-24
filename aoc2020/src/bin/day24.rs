8d9d76dad0f971792007c1ad91fb5e514152c0cd
Kejia Hu <kejia.hu@codethink.co.uk>
2020-12-24 07:36:12 +0000

    day24: initial

---
+++ aoc2020/src/bin/day24.rs
@@ -0,0 +1,64 @@
+use std::error::Error;
+
+use aoc2020::*;
+
+fn question2(data: Vec<String>) -> Result<usize, &'static str> {
+    Err("Cannot find second number.")
+}
+
+fn question1(data: Vec<String>) -> Result<usize, &'static str> {
+    Err("Cannot find first number.")
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let data = load_file()?;
+    println!("{:#?}", data);
+    match question1(data.to_owned()) {
+        Ok(x) => println!("The result for question 1 is {}", x),
+        Err(x) => eprintln!("Error processing the input data: {:?}", x),
+    };
+    match question2(data) {
+        Ok(x) => println!("The result for question 2 is {}", x),
+        Err(x) => eprintln!("Error processing the input data: {:?}", x),
+    };
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static TEST_INPUT: &str = r"sesenwnenenewseeswwswswwnenewsewsw
+    neeenesenwnwwswnenewnwwsewnenwseswesw
+    seswneswswsenwwnwse
+    nwnwneseeswswnenewneswwnewseswneseene
+    swweswneswnenwsewnwneneseenw
+    eesenwseswswnenwswnwnwsewwnwsene
+    sewnenenenesenwsewnenwwwse
+    wenwwweseeeweswwwnwwe
+    wsweesenenewnwwnwsenewsenwwsesesenwne
+    neeswseenwwswnwswswnw
+    nenwswwsewswnenenewsenwsenwnesesenew
+    enewnwewneswsewnwswenweswnenwsenwsw
+    sweneswneswneneenwnewenewwneswswnese
+    swwesenesewenwneswnwwneseswwne
+    enesenwswwswneneswsenwnewswseenwsese
+    wnwnesenesenenwwnenwsewesewsesesew
+    nenewswnwewswnenesenwnesewesw
+    eneswnwswnwsenenwnwnwwseeswneewsenese
+    neswnwewnwnwseenwseesewsenwsweewe
+    wseweeenwnesenwwwswnew";
+
+    #[test]
+    fn test_question1() {
+        let data: Vec<String> = TEST_INPUT.lines().map(|s| s.trim().to_owned()).collect();
+
+        assert_eq!(Ok(10), question1(data));
+    }
+    #[test]
+    fn test_question2() {
+        let data: Vec<String> = TEST_INPUT.lines().map(|s| s.trim().to_owned()).collect();
+
+        assert_eq!(Err("Cannot find second number."), question2(data));
+    }
+}
